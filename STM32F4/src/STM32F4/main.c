a92a37ae7faa04c4ee6aa5933caa7899cc4a5a22
GCY <gcy626@gmail.com>
2019-07-21 18:59:58 +0800

    STM32 VCP device example

---
+++ STM32F4/src/STM32F4/main.c
@@ -0,0 +1,247 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <stm32f4xx.h>
+#include <stm32f4xx_gpio.h>
+#include <stm32f4xx_usart.h>
+#include <stm32f4xx_exti.h>
+#include <stm32f4xx_i2c.h>
+#include <stm32f4xx_adc.h>
+#include <stm32f4xx_rcc.h>
+#include <stm32f4xx_dma.h>
+#include <stm32f4xx_rtc.h>
+
+
+#include "./usb_cdc_device/usbd_usr.h"
+#include "./usb_cdc_device/usbd_cdc_core.h"
+#include "./usb_cdc_device/usb_conf.h"
+#include "./usb_cdc_device/usbd_desc.h"
+#include "./usb_cdc_device/usbd_cdc_vcp.h"
+
+#include "tiny_printf.h"
+
+volatile uint32_t TimingDelay;
+
+volatile uint32_t micros = 0;
+
+void Delay(__IO uint32_t nTime)
+{
+   TimingDelay = nTime;
+   while(TimingDelay){
+   }
+}
+
+void SysTick_Handler(void)
+{
+   if(TimingDelay){
+      --TimingDelay;
+   }
+   ++micros;
+}
+
+/*
+ * APB2 = 84MHz, Div4, ADC CLOCK = 21MHz, 12cycles ( 12-bits resolution )
+ * (3clock + 12clock)/21M = 0.7us
+ * ADC_TwoSamplingDelay = 5clock/21MHz = 0.24us
+ * 0.7+0.24 = 0.94us = 1MHz sampling rate
+ */
+volatile uint32_t adc_value[4] = {0};
+void Init_ADC(void)
+{
+   GPIO_InitTypeDef gpio;
+   DMA_InitTypeDef dma;
+   NVIC_InitTypeDef nvic;
+   ADC_InitTypeDef adc;
+   ADC_CommonInitTypeDef adc_com;
+
+   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2 | RCC_AHB1Periph_GPIOC,ENABLE);
+   RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC3,ENABLE);
+
+   dma.DMA_Channel = DMA_Channel_2;
+   dma.DMA_PeripheralBaseAddr = (uint32_t)0x4001224C;
+   dma.DMA_Memory0BaseAddr = (uint32_t)&adc_value;
+   dma.DMA_DIR = DMA_DIR_PeripheralToMemory;
+   dma.DMA_BufferSize = 4;
+   dma.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+   dma.DMA_MemoryInc = DMA_MemoryInc_Enable;
+   dma.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Word;
+   dma.DMA_MemoryDataSize = DMA_MemoryDataSize_Word;
+   dma.DMA_Mode = DMA_Mode_Circular;
+   dma.DMA_Priority = DMA_Priority_High;
+   dma.DMA_FIFOMode = DMA_FIFOMode_Disable;
+   dma.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;
+   dma.DMA_MemoryBurst = DMA_MemoryBurst_Single;
+   dma.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
+   DMA_Init(DMA2_Stream0, &dma);
+   DMA_Cmd(DMA2_Stream0, ENABLE);
+
+   gpio.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
+   gpio.GPIO_Mode = GPIO_Mode_AN;
+   gpio.GPIO_PuPd = GPIO_PuPd_NOPULL;
+   GPIO_Init(GPIOC,&gpio);
+
+   adc_com.ADC_Mode = ADC_Mode_Independent;
+   adc_com.ADC_Prescaler = ADC_Prescaler_Div4;
+   adc_com.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
+   adc_com.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;
+   ADC_CommonInit(&adc_com);
+
+   adc.ADC_Resolution = ADC_Resolution_12b;
+   adc.ADC_ScanConvMode = ENABLE;
+   adc.ADC_ContinuousConvMode = ENABLE;
+   adc.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+   adc.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;
+   adc.ADC_DataAlign = ADC_DataAlign_Right;
+   adc.ADC_NbrOfConversion = 4;
+   ADC_Init(ADC3,&adc);
+
+   ADC_RegularChannelConfig(ADC3,ADC_Channel_10,1,ADC_SampleTime_3Cycles);
+   ADC_RegularChannelConfig(ADC3,ADC_Channel_11,2,ADC_SampleTime_3Cycles);
+   ADC_RegularChannelConfig(ADC3,ADC_Channel_12,3,ADC_SampleTime_3Cycles);
+   ADC_RegularChannelConfig(ADC3,ADC_Channel_13,4,ADC_SampleTime_3Cycles);
+   ADC_DMARequestAfterLastTransferCmd(ADC3,ENABLE);
+   ADC_Cmd(ADC3,ENABLE);
+   ADC_SoftwareStartConv(ADC3);
+   ADC_DMACmd(ADC3,ENABLE);
+
+
+}
+
+/* Private */
+#define USB_VCP_RECEIVE_BUFFER_LENGTH		32768
+uint8_t INT_USB_VCP_ReceiveBuffer[USB_VCP_RECEIVE_BUFFER_LENGTH];
+uint32_t int_usb_vcp_buf_in, int_usb_vcp_buf_out, int_usb_vcp_buf_num;
+USB_VCP_Result USB_VCP_INT_Status;
+//extern LINE_CODING linecoding;
+uint8_t USB_VCP_INT_Init = 0;
+USB_OTG_CORE_HANDLE	USB_OTG_dev;
+
+extern uint8_t INT_USB_VCP_ReceiveBuffer[USB_VCP_RECEIVE_BUFFER_LENGTH];
+
+USB_VCP_Result USBVCPInit(void)
+{
+   USBD_Init(&USB_OTG_dev,
+#ifdef USE_USB_OTG_FS
+	 USB_OTG_FS_CORE_ID,
+#else
+	 USB_OTG_HS_CORE_ID,
+#endif
+	 &USR_desc, 
+	 &USBD_CDC_cb, 
+	 &USR_cb);   
+
+   /* Reset buffer counters */
+   int_usb_vcp_buf_in = 0;
+   int_usb_vcp_buf_out = 0;
+   int_usb_vcp_buf_num = 0;
+
+   /* Initialized */
+   USB_VCP_INT_Init = 1;
+
+   return USB_VCP_OK;
+}
+
+USB_VCP_Result USB_VCP_GetStatus(void) {
+   if (USB_VCP_INT_Init) {
+      return USB_VCP_INT_Status;
+   }
+   return USB_VCP_ERROR;
+}
+
+USB_VCP_Result USB_VCP_Getc(uint8_t* c) {
+   /* Any data in buffer */
+   if (int_usb_vcp_buf_num > 0) {
+      /* Check overflow */
+      if (int_usb_vcp_buf_out >= USB_VCP_RECEIVE_BUFFER_LENGTH) {
+	 int_usb_vcp_buf_out = 0;
+      }
+      *c = INT_USB_VCP_ReceiveBuffer[int_usb_vcp_buf_out];
+      INT_USB_VCP_ReceiveBuffer[int_usb_vcp_buf_out] = 0;
+
+      /* Set counters */
+      int_usb_vcp_buf_out++;
+      int_usb_vcp_buf_num--;
+
+      /* Data OK */
+      return USB_VCP_DATA_OK;
+   }
+   *c = 0;
+   /* Data not ready */
+   return USB_VCP_DATA_EMPTY;
+}
+
+USB_VCP_Result USB_VCP_Putc(volatile char c) {
+   uint8_t ce = (uint8_t)c;
+
+   /* Send data over USB */
+   VCP_DataTx(&ce, 1);
+
+   /* Return OK */
+   return USB_VCP_OK;
+}
+
+USB_VCP_Result USB_VCP_Puts(char* str) {
+   while (*str) {
+      USB_VCP_Putc(*str++);
+   }
+
+   /* Return OK */
+   return USB_VCP_OK;
+}
+
+USB_VCP_Result INT_USB_VCP_AddReceived(uint8_t c) {
+   /* Still available data in buffer */
+   if (int_usb_vcp_buf_num < USB_VCP_RECEIVE_BUFFER_LENGTH) {
+      /* Check for overflow */
+      if (int_usb_vcp_buf_in >= USB_VCP_RECEIVE_BUFFER_LENGTH) {
+	 int_usb_vcp_buf_in = 0;
+      }
+      /* Add character to buffer */
+      INT_USB_VCP_ReceiveBuffer[int_usb_vcp_buf_in] = c;
+      /* Increase counters */
+      int_usb_vcp_buf_in++;
+      int_usb_vcp_buf_num++;
+
+      /* Return OK */
+      return USB_VCP_OK;
+   }
+
+   /* Return Buffer full */
+   return USB_VCP_RECEIVE_BUFFER_FULL;
+}
+
+int main(void)
+{
+   if(SysTick_Config(SystemCoreClock / 1000 / 1000)){
+      while(1){}
+   }
+
+   USBVCPInit();
+   Init_ADC();
+
+   bool adc_out = false;
+   while(1){
+
+      unsigned char str[255];
+
+      sprintf(str,"[%d,%d,%d,%d,%d]\r\n",micros,adc_value[0],adc_value[1],adc_value[2],adc_value[3]);  
+
+      if(USB_VCP_GetStatus() == USB_VCP_CONNECTED) {
+	 uint8_t c;
+	 if (USB_VCP_Getc(&c) == USB_VCP_DATA_OK) {
+	    /* Return data back */
+	    if(c == 0xC){
+	       adc_out ^= true;
+	    }	    
+	    USB_VCP_Putc(c);
+	 }	
+	 else if(adc_out){
+	    USB_VCP_Puts(str);
+	 }
+      }
+
+      Delay(100);
+   }
+
+   return(0); // System will implode
+}    
